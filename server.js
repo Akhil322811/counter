306657d3bd6ed2dd6e13bc44f8f7f27a3c148e2d
Akhil322811 <150580744+Akhil322811@users.noreply.github.com>
2023-11-24 21:24:44 +0530

    Add files via upload
    counter by using js

---
+++ server.js
@@ -0,0 +1,12 @@
+const disvalue=document.getElementById("disValue")
+let count = 0
+
+
+function Increment(){
+    count = count+1;
+    disvalue.innerHTML=count
+}
+function Decrement(){
+    count = count-1;
+    disvalue.innerHTML=count
+}
